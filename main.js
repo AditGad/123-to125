16fd6e3724352eac560d90313792a2b6c42bb182
AditGad <79444282+AditGad@users.noreply.github.com>
2021-10-15 14:00:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,16 @@ posenet.on("pose",getposes);
 function modelloaded(){
     console.log("posenetstarted");
 }
+function getposes(results){
+    console.log(results);
+    leftwrist=results[0].pose.leftWrist.x;
+    rightwrist=results[0].pose.rightWrist.x;
+    diffrence=floor(leftwrist-rightwrist);
+    }
+    function draw(){
+        background('#969A97');
+        document.getElementById("fontisize").innerHTML="Font Size Is "+diffrence+"px";
+        textSize(diffrence);
+        fill('#FFE787')
+        text("Adit.C.Gad",50,400);
+    }
